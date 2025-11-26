cfaf85bc4632401093cd41c9515c769f9b69bac6
ChrisOspina <ChrisOspina99@gmail.com>
2025-11-26 17:27:15 -0500

    Header created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
+      <Header />
       <Home />
       <footer>
         <Footer />
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 py-2">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        <a href={"/"}>
+          <img
+            width={150}
+            height={50}
+            className="h-12 py-1 w-auto object-contain"
+            src="./anipsobw.png"
+            alt="anipso logo"
+          />
+        </a>
+      </div>
+      <div className="flex gap-4"></div>
+    </header>
+  );
+};
+
+export default Header;
