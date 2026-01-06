f1d4479b8e36c39ba3c8c2a3d40f7a1537188a6f
ChrisOspina <ChrisOspina99@gmail.com>
2026-01-06 16:41:10 -0500

    put weatherApi inside useEffect hook

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,7 +43,7 @@ const Home = () => {
       setIsLoading(false);
     };
     navigator.geolocation.getCurrentPosition(positionSuccess, positionError);
-  }, []);
+  }, [weatherApi]);
 
   return (
     <div
